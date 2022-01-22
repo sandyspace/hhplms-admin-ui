7ecfc4a66fe7b15128477605d544ddad331fb0d4
山成 <shanc@133.cn>
2022-01-22 16:26:03 +0800

    修改测试和生产环境域名配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"https://www.iotecloud.com/api"',
-  UPLOADED_IMG_BASE_PATH: '"https://www.iotecloud.com/client/cbgj/upload/"',
+  BASE_API: '"https://cssc.iotecloud.cn/api"',
+  UPLOADED_IMG_BASE_PATH: '"https://cssc.iotecloud.cn/client/cbgj/upload/"',
   ASSETS_PUBLIC_PATH: '"/client/cbgj/admin/"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  BASE_API: '"http://test.iotecloud.com/api"',
-  UPLOADED_IMG_BASE_PATH: '"http://test.iotecloud.com/client/cbgj/upload/"',
+  BASE_API: '"http://cssc-test.iotecloud.cn/api"',
+  UPLOADED_IMG_BASE_PATH: '"http://cssc-test.iotecloud.cn/client/cbgj/upload/"',
   ASSETS_PUBLIC_PATH: '"/client/cbgj/admin/"'
 }
